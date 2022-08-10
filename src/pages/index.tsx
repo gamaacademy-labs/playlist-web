94a5dacd2ce61977f55eee6d1888e2a8a122f843
mendex94 <lfelipecmendes@icloud.com>
2022-08-10 15:29:16 -0300

    feat/tailwindcss-setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable linebreak-style */
+
 export default function Home() {
-  return (
-    <div>
-      Ola mundo
-    </div>
-  )
+	return (
+		<div className='bg-slate-600 container mx-auto md:mx-0'>
+			<h1 className='text-2xl text-red-900'>Hello World</h1>
+		</div>
+	)
 }
