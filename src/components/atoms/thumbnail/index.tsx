7c60a65122b8b9b3e839627344d8d87f2fa7c21b
mendex94 <lfelipecmendes@icloud.com>
2022-08-12 18:09:57 -0300

    fix/fixing-thumbnail-component

--- src/components/atoms/thumbnail/index.tsx
+++ src/components/atoms/thumbnail/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import { Tag } from '@gama-academy/smash-web'
 
 function ImgThubnail() {
 	return (
-		<div className='bg-[#7D38DB] rounded-lg p-2 flex items-center justify-center'>
+		<div className='bg-[#7D38DB] rounded-lg p-2 flex items-center justify-center relative'>
 			<Image 
 				src='https://cdn.discordapp.com/attachments/1006677588242088016/1007389202641588415/HA04_HTML.jpg'
 				alt="Image thumbnail"
@@ -11,6 +12,9 @@ function ImgThubnail() {
 				height={150}
 				width={325}
 			/>
+			<div className='absolute right-5 top-5'>
+				<p className='bg-[#7D38DB] text-white px-1 rounded'>PLAYLIST</p>
+			</div>
 		</div>
 	)
 }
