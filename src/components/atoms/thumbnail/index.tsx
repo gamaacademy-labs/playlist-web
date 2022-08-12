919dec34f0615270f6fa5dfe2c22ef9f8188f206
mendex94 <lfelipecmendes@icloud.com>
2022-08-12 17:36:59 -0300

    fix/fixing-thumbnail-component

--- src/components/atoms/thumbnail/index.tsx
+++ src/components/atoms/thumbnail/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import Thumbnail from '../../../assets/images/imageThumb.png'
-
-
 
 function ImgThubnail() {
 	return (
-		<div>
+		<div className='bg-[#7D38DB] rounded-lg p-2 flex items-center justify-center'>
 			<Image 
-				src={Thumbnail}
+				src='https://cdn.discordapp.com/attachments/1006677588242088016/1007389202641588415/HA04_HTML.jpg'
 				alt="Image thumbnail"
-				className='object-cover'
-				layout='responsive'
+				className='rounded-lg object-cover'
+				height={150}
+				width={325}
 			/>
 		</div>
 	)
