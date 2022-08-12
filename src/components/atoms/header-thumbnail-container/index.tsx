eeaa5352c9c962d2250f4e10b6177afb80dbb76e
mendex94 <lfelipecmendes@icloud.com>
2022-08-12 17:56:13 -0300

    fix/fixing-thumbnail-component

--- src/components/atoms/header-thumbnail-container/index.tsx
+++ src/components/atoms/header-thumbnail-container/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderThumbnailContainer  ({children}) {
 	return (
-		<div className='p-6 flex' >
+		<div className='p-6 flex items-center gap-2' >
 			{children}
 		</div>
 	)
