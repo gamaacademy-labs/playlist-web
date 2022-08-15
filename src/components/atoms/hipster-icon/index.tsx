496dfdc40f2363b9fe8f38c3b8abca5a0933adb0
karennabarrete <karen.nabarrete.garcia@gmail.com>
2022-08-15 15:27:18 -0300

    feat/title-rating component

---
+++ src/components/atoms/hipster-icon/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HipsterIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      fill="none"
+      viewBox="0 0 24 25"
+    >
+      <rect width="24" height="24" y="0.5" fill="#68DE5A" rx="12"></rect>
+      <path
+        fill="#121212"
+        d="M18.086 10.551l.952 1.11-.9.85h-1.043l-3.09-2.011-2.006 1.047L9.994 10.5l-3.089 2.012H5.86l-.9-.851.952-1.11L4 11.507v1.97L6.014 15.5h2.814l3.17-2.063 3.174 2.063h2.812L20 13.477v-1.97l-1.914-.956z"
+      ></path>
+    </svg>
+  );
+}
+
+export default HipsterIcon;
