f5da22c8f1754404e2b9512d0a3251b6231429f0
robkbr <roberto.junior@nickelpay.com.br>
2022-08-22 12:53:27 -0300

    fix: remove broken import

--- src/components/templates/main.tsx
+++ src/components/templates/main.tsx
@@ -1,24 +1,22 @@
 import Head from 'next/head'
-import Navbar from '../molecules/navbar'
 import React from 'react'
-import BackgroundButtonPlaylist from '../atoms/backgroud-button-playlist'
+import Navbar from '../molecules/navbar'
 
 interface Props {
   children: React.ReactNode
 }
 
-const Main: React.FC<Props> = ({children}) => {
-	return( 
-		<>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>Gama Academy - Playlists</title>
-			</Head>
-			<Navbar/>
-			{children}
-			
-		</>
-	)
+const Main: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Gama Academy - Playlists</title>
+      </Head>
+      <Navbar />
+      {children}
+    </>
+  )
 }
 
 export default Main
