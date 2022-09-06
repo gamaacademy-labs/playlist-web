ba460725f5d1188d1d4e3d1df241f388b896eb7a
EnioSFilho <eniofilho76@gmail.com>
2022-09-06 15:32:53 -0300

    feat/playlist75

--- src/pages/playlist75.tsx
+++ src/pages/playlist75.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next'
 import DetailsHeader from '../components/atoms/details-header'
+import DownloadCertificateButtonGreen from '../components/atoms/download-certificate-button-green'
 import PlaybuttonContainer from '../components/atoms/playbutton-container'
 import ProgressBarButton from '../components/atoms/progress-bar-button'
 import RefreshButton from '../components/atoms/refresh-button'
@@ -13,11 +14,12 @@ import Thumbnail from '../components/molecules/thumbnail'
 import Tabs from '../components/tabs'
 import Layout from '../components/templates/motion'
 
-export default function Home({ playlist }) {
+export default function Playlist75({ playlist }) {
   return (
     <Layout>
       <PlaylistHeader />
-        <div className="md:flex md:justify-between">
+
+      <div className="md:flex md:justify-between">
         <Thumbnail />
         <div className="hidden md:inline order-first md:max-h-16">
           <PlaylistTitle />
@@ -30,10 +32,11 @@ export default function Home({ playlist }) {
         <PlaylistTitle />
 
       </div>
-      <div className="md:hidden">
-        <TabContainer>
-          <Tabs contents={playlist.contents} />
-        </TabContainer>
+      <div className="text-violet-700 md:hidden">
+          <p style={{color: "#7d38db", marginLeft: 25}}>CERTIFICADO DISPONÍVEL</p>
+          <div className="ml-5 mt-5">
+          <DownloadCertificateButtonGreen/>
+          </div>
       </div>
       <a href="/aula1"><RefreshButton contents={playlist.contents} /></a>
     </Layout>
