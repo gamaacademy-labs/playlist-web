00046e04e887516cb83b300c2cb8bf6864c89395
karennabarrete <karen.nabarrete.garcia@gmail.com>
2022-09-15 12:37:31 -0300

    feat: responsive class

--- src/components/atoms/video/index.tsx
+++ src/components/atoms/video/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Video({ url }) {
   return (
-    <div>
-      <iframe width="560" height="315" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <div className='flex w-96 h-52' >
+      <iframe src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
     </div>
   )
